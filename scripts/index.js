5f54152cc7ae3c9d148457b230e14f509c4d61a2
Tong <tongjeeeung@gmail.com>
2023-12-16 17:15:43 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,38 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placeList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard(element) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = `${element.link}`;
+  cardElement.querySelector('.card__title').textContent = `${element.name}`;
+
+  const removeButton = cardElement.querySelector('.card__delete-button');
+  removeButton.addEventListener('click', function() {
+    removeCard(element.name);
+  })
+
+  return cardElement
+}
+
+function removeCard(name) {
+  let i = 0;
+  cards.forEach((element) => {
+    if(element.querySelector('.card__title').textContent === name) {
+      cards[i].remove();
+      i = 0;
+    }
+    else {
+      i += 1;
+    }
+  })
+}
+
+initialCards.forEach((element) => {
+  let card = createCard(element);
+  placeList.append(card);
+})
+
+const cards = document.querySelectorAll('.card');
