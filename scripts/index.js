19de5e2842d3bf399b93daae83164a521cfb3801
Tong <tongjeeeung@gmail.com>
2023-12-16 20:23:12 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -7,38 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-const placeList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-function createCard(element) {
+function createCard(cardData, removeCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = `${element.link}`;
-  cardElement.querySelector('.card__title').textContent = `${element.name}`;
-
   const removeButton = cardElement.querySelector('.card__delete-button');
-  removeButton.addEventListener('click', function() {
-    removeCard(element.name);
-  })
+
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  removeButton.addEventListener('click', () => removeCard(cardElement));
 
   return cardElement
 }
 
-function removeCard(name) {
-  let i = 0;
-  cards.forEach((element) => {
-    if(element.querySelector('.card__title').textContent === name) {
-      cards[i].remove();
-      i = 0;
-    }
-    else {
-      i += 1;
-    }
-  })
+function removeCard(card) {
+  card.remove()
 }
 
-initialCards.forEach((element) => {
-  let card = createCard(element);
-  placeList.append(card);
-})
-
-const cards = document.querySelectorAll('.card');
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, removeCard);
+  cardsContainer.append(card);
+});
